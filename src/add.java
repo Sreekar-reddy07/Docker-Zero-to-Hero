05d64c4f8a5d380b25b8bf9170c4d3bdff8714f6
Sreekar-reddy07 <sreekarreddykarnati@gmail.com>
2025-02-05 15:44:12 -0500

    Create add.java

---
+++ src/add.java
@@ -0,0 +1,13 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        // Declaring numbers
+        int num1 = 5;
+        int num2 = 10;
+
+        // Multiplying the numbers
+        int result = num1 * num2;
+
+        // Displaying the result
+        System.out.println("The product is: " + result);
+    }
+}
