46aa781b681e02f84f6dd6fddc9b81b12f4f9547
Sreekar-reddy07 <sreekarreddykarnati@gmail.com>
2025-02-05 15:41:44 -0500

    Update add.py

--- src/add.py
+++ src/add.py
@@ -11,12 +11,3 @@ sum_result = add(num1, num2)
 
 # Displaying the result
 print("The sum is:", sum_result)
-
-# Testing function
-def test_add():
-    assert add(1, 4) == 5
-    assert add(1, -2) == -1
-
-# Run the test
-test_add()
-print("All tests passed successfully!")
