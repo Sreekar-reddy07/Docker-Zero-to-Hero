1f6d229ab93caa515125cf3040b43b503fb85f0a
Sreekar-reddy07 <sreekarreddykarnati@gmail.com>
2025-02-05 19:09:44 -0500

    Create app.py

---
+++ docker/app.py
@@ -0,0 +1,9 @@
+# add.py
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 20
+    result = add(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {result}")
