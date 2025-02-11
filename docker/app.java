f27049c26e6a9aa81173a76560cf43468d029b19
Sreekar-reddy07 <sreekarreddykarnati@gmail.com>
2025-02-10 20:07:11 -0500

    java program to add and sub

---
+++ docker/app.java
@@ -0,0 +1,13 @@
+public class SimpleMath 
+{
+    public static void main(String[] args) 
+    {
+        // Addition of 35 and 65
+        int sum = 35 + 65;
+        System.out.println("Sum of 35 and 65: " + sum);
+
+        // Subtraction of 100 and 85
+        int difference = 100 - 85;
+        System.out.println("Difference of 100 and 85: " + difference);
+    }
+}
